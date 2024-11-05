9fca0b0fe9e2ba53b90a701fd83dcf0ac59143b8
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2024-11-05 18:08:41 +0300

    added ipynb and base code

--- aai_lab1/main.py
+++ aai_lab1/main.py
@@ -1,38 +1,68 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 def read_data(path, filename):
     return pd.read_csv(os.path.join(path, filename))
 
 
-def load_dataset(path: str, label_dict):
-    train_X = read_data(path, 'train.csv').values[:,:-2]
-    train_y = read_data(path, 'train.csv')['Activity']
+def load_dataset(path: str, train_file: str, test_file: str, row: str,
+                 label_dict):
+    train_X = read_data(path, train_file).values[:, :-2]
+    train_y = read_data(path, train_file)[row]
     train_y = train_y.map(label_dict).values
-    test_X = read_data(path, 'test.csv').values[:,:-2]
-    test_y = read_data(path, 'test.csv')
-    test_y = test_y['Activity'].map(label_dict).values
+    test_X = read_data(path, test_file).values[:, :-2]
+    test_y = read_data(path, test_file)
+    test_y = test_y[row].map(label_dict).values
     return (train_X, train_y, test_X, test_y)
 
 
-path = 'aai_lab1/'
+path = 'D:/code/aai-labs/aai_lab1/'
+train_file = 'train.csv'
+test_file = 'test.csv'
+row = 'Activity'
 
-df = read_data(path, 'train.csv')
+df = read_data(path, train_file)
 df.head()
 
-label_dict = {'WALKING':0, 'WALKING_UPSTAIRS':1, 'WALKING_DOWNSTAIRS':2, 'SITTING':3, 'STANDING':4, 'LAYING':5}
-train_X, train_y, test_X, test_y = load_dataset(path, label_dict)
+label_dict = {
+    'WALKING': 0,
+    'WALKING_UPSTAIRS': 1,
+    'WALKING_DOWNSTAIRS': 2,
+    'SITTING': 3,
+    'STANDING': 4,
+    'LAYING': 5
+}
+train_X, train_y, test_X, test_y = load_dataset(path, train_file, test_file,
+                                                row, label_dict)
 
-model = KNeighborsClassifier(n_neighbors=3)
+model = LogisticRegression(max_iter=1000, multi_class='ovr')
 model.fit(train_X, train_y)
 
 yhat = model.predict(test_X)
 
-target_names = ['Walking', 'Walking Upstairs', 'Walking Downstairs', 'Sitting', 'Standing', 'Laying']
+target_names = [
+    'Walking', 'Walking Upstairs', 'Walking Downstairs', 'Sitting', 'Standing',
+    'Laying'
+]
 
 print(classification_report(test_y, yhat, target_names=target_names))
+
+conf_matrix = confusion_matrix(test_y, yhat)
+
+plt.figure(figsize=(10, 8))
+heatmap = sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=target_names, yticklabels=target_names)
+heatmap.set_xticklabels(heatmap.get_xticklabels(), rotation=20)
+heatmap.set_yticklabels(heatmap.get_yticklabels(), rotation=60)
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Confusion Matrix')
+plt.show()
